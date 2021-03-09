d95f0ffb2947c9a05afe4b8ec6ce1e8e34dedb44
AuM0b <50074639+AuM0b@users.noreply.github.com>
2021-03-10 05:12:23 +0700

    Initial commit

---
+++ GobbleGumPicker/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GobbleGum Picker")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("AuM0b")]
+[assembly: AssemblyProduct("GobbleGum Picker")]
+[assembly: AssemblyCopyright("Copyright © AuM0b 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("83193e75-a49a-453c-9840-5d68f59acee5")]
+
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: NeutralResourcesLanguage("en")]
+
